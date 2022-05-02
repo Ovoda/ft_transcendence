770451dec07dfcb98dc39d54a05654064576dfd1
Nastasia Demont <nastasia@demont.me>
2022-05-02 18:12:08 +0200

    It works en dur

--- srcs/requirements/backend/src/app.module.ts
+++ srcs/requirements/backend/src/app.module.ts
@@ -2,30 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { ChatModule } from './chat/chat.module';
-import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [
-	ConfigModule.forRoot ({
-        envFilePath: '../.env',
-        isGlobal: true,
-    }),
-	TypeOrmModule.forRoot({
-		type: 'postgres',
-		host: process.env.POSTGRES_HOST,
-		port: parseInt(<string>process.env.POSTGRES_PORT),
-		database: process.env.POSTGRES_DB,
-		username: process.env.POSTGRES_USER,
-		password: process.env.POSTGRES_PASSWORD,
-		synchronize: true,
-		entities: [],
-	}),
-	GameModule,
-	ChatModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host:'localhost',
+			port: 5432,
+			database: 'pong',
+			username: 'wolfpack',
+			password: 'password',
+			entities: [],
+			synchronize: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
