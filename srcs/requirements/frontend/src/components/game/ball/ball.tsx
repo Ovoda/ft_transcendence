ac49e492d793bbc6bb072ba281ac777b5562a436
gpetit <gpetit@student.42.fr>
2022-05-09 10:34:56 +0200

    new functions organization in game and use of circle functions instead of rectangle

--- srcs/requirements/frontend/src/components/game/ball/ball.tsx
+++ srcs/requirements/frontend/src/components/game/ball/ball.tsx
@@ -1,10 +1,9 @@
-import drawPlayers from '../player/player';
 
-class Ball {
+export default class Ball {
 	radius;
 	position;
 	velocity;
-	constructor() {
+	constructor(c : CanvasRenderingContext2D) {
 		this.position = {
 			x: 150,
 			y: 70
@@ -15,29 +14,18 @@ class Ball {
 			y: 0
 		}
 	}
-	draw(c: CanvasRenderingContext2D) {
-		c?.fillRect(this.position.x, this.position.y, this.radius, this.radius)
+	draw(c : CanvasRenderingContext2D) {
+		c?.beginPath();
+		c?.arc(this.position.x, this.position.y, 10, 0, 2 * Math.PI);
+		c?.fill();
+		c?.stroke();
 	}
-	update(c: CanvasRenderingContext2D) {
-		this.draw(c);
-		this.position.x += this.velocity.x;
+	update(c : CanvasRenderingContext2D) {
+		this.draw(c)
+		this.position.x += this.velocity.x
 		if (this.position.x + this.radius + this.velocity.x > 300)
 			this.velocity.x = -(this.velocity.x);
 		else if (this.position.x === 0)
 			this.velocity.x = -(this.velocity.x);
-	}
-}
-
-const ball = new Ball();
-
-export default function animate() {
-	let canvas = document.querySelector('canvas');
-	let c = canvas?.getContext('2d');
-	console.log(canvas);
-	requestAnimationFrame(animate);
-	c?.clearRect(0, 0, 1000, 700);
-	if (c) {
-		ball.update(c);
-		drawPlayers(c);
 	}
 }
--- srcs/requirements/frontend/src/components/game/game.tsx
+++ srcs/requirements/frontend/src/components/game/game.tsx
@@ -1,17 +1,35 @@
-import animate from './ball/ball';
+import Ball from './ball/ball';
+import drawPlayers from './player/player';
 import './game.scss';
 import React, { useEffect } from 'react';
 
+let canvas = document.querySelector('canvas');
+let c = canvas?.getContext('2d');
+
 class Score {
-	player1;
-	player2;
-	constructor() {
-		this.player1 = 0
-		this.player2 = 0
-	}
+	constructor(public player1: number, public player2 : number) {}
 }
 
-const score = new Score()
+const score = new Score(0, 0);
+
+const app = document.getElementById("output");
+const p = document.createElement("p");
+p.textContent = score.player1.toString() + " : " + score.player2.toString();
+app?.appendChild(p);
+
+//proteger contre type undefined or Null
+const ball = new Ball(c!);
+
+function animate() {
+
+	console.log(canvas);
+	requestAnimationFrame(animate)
+	c?.clearRect(0, 0, 1000, 700)
+	if (c) {
+		ball.update(c);
+		drawPlayers(c);
+	}
+}
 
 function Game() {
 	//useEffect(() => {
