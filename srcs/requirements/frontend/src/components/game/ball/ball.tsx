a9427513c9425ccecf789dae001761666f517d69
Nastasia Demont <nastasia@demont.me>
2022-05-10 11:22:13 +0200

    Add line in the middle

--- srcs/requirements/frontend/src/components/game/ball/ball.tsx
+++ srcs/requirements/frontend/src/components/game/ball/ball.tsx
@@ -2,9 +2,10 @@ import { Player } from '../player/player';
 
 function updateScore(c: CanvasRenderingContext2D, canvas : HTMLCanvasElement, player1 : Player, player2 : Player)
 {
-	let str : string = player1.score.toString() + " : " + player2.score.toString()
+	let str : string = player1.score.toString() + "   " + player2.score.toString()
 	c.font = "10px Arial"
 	c.fillText(str, (canvas.width / 2) - 10, 10)
+	c?.fillRect((canvas.width / 2) - 1, 0, 1, canvas.height)
 }
 
 function playerColision(ball : Ball, player1: Player, player2: Player) {
