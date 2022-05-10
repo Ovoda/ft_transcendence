b01a84185fdec24610b603a905883e406440941a
Nastasia Demont <nastasia@demont.me>
2022-05-10 11:34:24 +0200

    Bounce on walls. But ball movement not on point

--- srcs/requirements/frontend/src/components/game/ball/ball.tsx
+++ srcs/requirements/frontend/src/components/game/ball/ball.tsx
@@ -8,19 +8,25 @@ function updateScore(c: CanvasRenderingContext2D, canvas : HTMLCanvasElement, pl
 	c?.fillRect((canvas.width / 2) - 1, 0, 1, canvas.height)
 }
 
+function hitWall(ball: Ball, canvas : HTMLCanvasElement) {
+	if (ball.position.y === 0 || ball.position.y === canvas.height)
+		return (1);
+	return (0);
+}
+
 function playerColision(ball : Ball, player1: Player, player2: Player) {
 	if (ball.position.x <= player1.position.x + player1.width
 		&& ball.position.y >= player1.position.y
 		&& ball.position.y <= player1.position.y + player1.height) {
-			return (1)
+			return (1);
 	}
 	else if (ball.position.x >= player2.position.x
 		&& ball.position.y >= player2.position.y
 		&& ball.position.y <= player2.position.y + player2.height) {
-			return (1)
+			return (1);
 	}
 	else {
-		return (0)
+		return (0);
 	}
 }
 
@@ -57,8 +63,16 @@ export class Ball {
 		this.draw(c);
 		updateScore(c, canvas, player1, player2)
 		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
 		if (playerColision(this, player1, player2))
+		{
 			this.velocity.x = -(this.velocity.x);
+			this.velocity.y = 1;
+		}
+		else if (hitWall(this, canvas)) {
+			this.velocity.y = -(this.velocity.y);
+
+		}
 		else if (this.position.x <= 0) {
 			player2.score += 1
 			if (player2.score >= 10)
