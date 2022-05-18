89f0a9a8110e764883ad4eaf4e135ce973978018
Nastasia Demont <nastasia@demont.me>
2022-05-18 09:12:21 +0200

    Merge to game, to merge to dev

---
+++ srcs/requirements/backend/src/app.module 2.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { configService } from './config/config.service';
+
+@Module({
+	imports: [
+		TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule { }
