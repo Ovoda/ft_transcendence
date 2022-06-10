7a6712095c10486744974b105b7a009d9b7ea6ff
nboisde <nboisde.d3v@gmail.com>
2022-06-10 12:21:48 +0200

    test upload file feature

--- backend/src/images/images.service.ts
+++ backend/src/images/images.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from "@nestjs/common";
+import { extname } from "path";
 
 @Injectable()
 export class ImagesService {
 	constructor() {}
+
+
+	imageFileFilter(req, file, callback) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+		  return callback(new Error('Only image files are allowed!'), false);
+		}
+		callback(null, true);
+	  };
+	
+	editFileName(req, file, callback) {
+		const name = file.originalname.split('.')[0];
+		const fileExtName = extname(file.originalname);
+		const randomName = Array(4)
+		  .fill(null)
+		  .map(() => Math.round(Math.random() * 16).toString(16))
+		  .join('');
+		callback(null, `${name}-${randomName}${fileExtName}`);
+	  };
 }
