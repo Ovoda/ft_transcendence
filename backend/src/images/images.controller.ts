a6a30cc694091d2ed0c99df1aa169b21dd227197
nboisde <nboisde.d3v@gmail.com>
2022-06-10 12:21:48 +0200

    test upload file feature

--- backend/src/images/images.controller.ts
+++ backend/src/images/images.controller.ts
@@ -1,31 +1,30 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
-import { ExpressAdapter, FileInterceptor } from "@nestjs/platform-express";
-import fs from 'fs';
-import multer, { diskStorage } from "multer";
-import path from "path";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { ImagesService } from "./images.service";
+import { editFileName, imageFileFilter } from "./utils/fileUpload.utils";
 
 @Controller('images')
 export class ImagesController {
-	constructor() {}
+	constructor(
+		private readonly imagesService: ImagesService,
+	) {}
 
-	// @UseInterceptors(FileInterceptor('file', {
-		// 	storage:{
-			// 	destination(req, file, cb) {
-				// 			cb(null, './uploads/')
-				// 	},
-				// 	filename(req, file, cb) {
-					// 	  cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-					// 	}
-					// }
-					// }))
-	@Post('upload')
-	//@UseInterceptors('file')
-	async uploadFile(@UploadedFile() file: Express.Multer.File) {
-		
+	  @Post('upload')
+	  @UseInterceptors(
+		FileInterceptor('image', {
+		  storage: diskStorage({
+			destination: './files',
+			filename: editFileName,
+		  }),
+		  fileFilter: imageFileFilter,
+		}),
+	  )
+	  async uploadedFile(@UploadedFile() file) {
 		const response = {
-			originalname: file.originalname,
-			filename: file.filename + '.png',
+		  originalname: file.originalname,
+		  filename: file.filename,
 		};
-		return response
-	}
+		return response;
+	  }
 }
