e67b4af8fa931e43ae09a2984208a70278b17d6d
nboisde <nboisde.d3v@gmail.com>
2022-06-13 22:39:17 +0200

    rebase

--- backend/src/user/dtos/updateUser.dto.ts
+++ backend/src/user/dtos/updateUser.dto.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { IsNotEmpty } from "class-validator";
 
 export default class UpdateUserDto {
 	@IsNotEmpty()
-=======
-
-export default class UpdateUserDto {
->>>>>>> d008819 (Profile)
     username: string;
 }
